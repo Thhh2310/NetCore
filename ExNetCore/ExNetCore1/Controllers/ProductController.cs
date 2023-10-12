d2f4f5771bd8a7e377a4cba6a2dd2db82276ecbd
Thhh2310 <vuthao23102003@gmail.com>
2023-10-12 21:36:25 +0700

    Update NetCore

---
+++ ExNetCore/ExNetCore1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExNetCore1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
